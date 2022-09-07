b041d5fdbe91e3015661add465eb85078bfc5589
Darshana Baruah <darsanabaruah@gmail.com>
2022-09-07 16:17:22 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import models
+from database import engine
+from routers import authentication
+from fastapi.middleware.cors import CORSMiddleware
+
+app=FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+models.Base.metadata.create_all(engine)
+app.include_router(authentication.router)
+
---
+++ routers/authentication.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter,Depends,status,HTTPException
+from routers import schemas
+import database,models
+from sqlalchemy.orm import Session
+router=APIRouter(tags=['Authentication'])
+
+
+@router.post('/register')
+def register(request:schemas.Register,db: Session=Depends(database.get_db)):
+    new_user = db.query(models.User).filter(
+        models.User.email == request.email).first()
+    if new_user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail=f"Already Exists")
+    else:
+        new_user = models.User(
+        name=request.name, email=request.email, password=request.password)
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+        return {"message":"user successfully created"}
+
+@router.post('/login')
+def login(request:schemas.Login,db: Session=Depends(database.get_db)):
+    user=db.query(models.User).filter(models.User.email==request.username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Invalid Credentials")
+    return user
